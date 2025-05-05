c3ef3eefeb4d059def6996825d36065b6c41c615
Noah <nzinn7734@gmail.com>
2025-05-05 19:09:35 -0400

    Add project

---
+++ src/main/java/com/nzinn/spaceevents/SpaceeventsApplication.java
@@ -0,0 +1,13 @@
+package com.nzinn.spaceevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceeventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceeventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nzinn/spaceevents/SpaceeventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nzinn.spaceevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceeventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
